d6cd463df70de79bfd83f8a61ab13ddff5104b00
Benny Degezelle <hi@monkeypatch.be>
2014-02-05 00:39:32 +0100

    Add radius for r:assets:first and r:if_assets overwrites

--- lib/clipped_asset_roles/asset_tag_extensions.rb
+++ lib/clipped_asset_roles/asset_tag_extensions.rb
@@ -4,6 +4,7 @@ module ClippedAssetRoles::AssetTagExtensions
   def self.included(base)
     base.class_eval {
       alias_method_chain :assets_find_options, :roles
+      alias_method_chain :find_asset, :roles
     }
   end
 
@@ -19,6 +20,46 @@ module ClippedAssetRoles::AssetTagExtensions
   }    
   tag 'assets' do |tag|
     tag.expand
+  end
+  
+  desc %{
+    References the first asset attached to the current page.  
+    
+    *Usage:* 
+    <pre><code><r:assets:first>...</r:assets:first></code></pre>
+  }
+  tag 'assets:first' do |tag|
+    if role = options.delete('role') && tag.locals.asset = tag.locals.page.attachments_with_role(role).first
+      tag.expand
+    elsif tag.locals.asset = tag.locals.page.assets.first
+      tag.expand
+    end
+  end
+  
+  desc %{
+    Renders the contained elements only if the current contextual page has one or
+    more assets. The @min_count@ attribute specifies the minimum number of required
+    assets. You can also filter by extensions with the @extensions@ or @roles@ attribute.
+  
+    *Usage:*
+    <pre><code><r:if_assets [min_count="n"]>...</r:if_assets></code></pre>
+  }
+  tag 'if_assets' do |tag|
+    count = tag.attr['min_count'] && tag.attr['min_count'].to_i || 1
+    assets = tag.locals.page.assets.count(:conditions => assets_find_options(tag)[:conditions])
+    tag.expand if assets >= count
+  end
+  
+  def find_asset_with_roles(tag, options)
+    begin
+      find_asset_without_roles
+    rescue TagError
+      if role = options.delete('role')
+        tag.locals.asset = tag.locals.page.attachments_with_role(role).first
+      else
+        raise(TagError, "Asset not found.")
+      end
+    end
   end
   
   def assets_find_options_with_roles(tag)
